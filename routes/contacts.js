771f6bfd488550713a7617505597b9be4fc1d3f6
448267450 <wenkun.ren@gmail.com>
2024-02-03 14:36:25 -0600

    data binding

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+// First Name, Last Name, Email Address
 let data = [
-  {},
-  {}
+  {firstName: 'Johnson', lastName: 'Sessions', emailAdd: 'johnson.sessions@gmail.com', 
+  id: '733fbcc6-6e69-4eb4-94f3-b71283bbbe37'},
+  {firstName: 'Leah', lastName: 'Dvozak', emailAdd: 'leah.dvozak@gmail.com', 
+  id: '4b88c966-a392-410d-becb-8f34ca07ba6a'}
 ];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('contacts', { title: 'Express Contacts' } );
+  res.render('contacts', { title: 'Express Contacts',  contacts: data } );
 });
 
 module.exports = router;
