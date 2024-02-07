c104ffec058bd22d634a51c130fe18b97406cc9c
448267450 <wenkun.ren@gmail.com>
2024-02-07 10:20:38 -0600

    create json file

--- src/contactsFileRepository.js
+++ src/contactsFileRepository.js
@@ -1,14 +1,24 @@
 const crypto = require('node:crypto');
 const fs = require('node:fs')
+const path = require('node:path')
 const db = new Map();
 
-db.set('733fbcc6-6e69-4eb4-94f3-b71283bbbe37',{firstName: 'Johnson', lastName: 'Sessions', emailAdd: 'johnson.sessions@gmail.com', 
-id: '733fbcc6-6e69-4eb4-94f3-b71283bbbe37'});
-db.set('4b88c966-a392-410d-becb-8f34ca07ba6a',{firstName: 'Leah', lastName: 'Dvozak', emailAdd: 'leah.dvozak@gmail.com', 
-id: '4b88c966-a392-410d-becb-8f34ca07ba6a'});
+// db.set('733fbcc6-6e69-4eb4-94f3-b71283bbbe37',{firstName: 'Johnson', lastName: 'Sessions', emailAdd: 'johnson.sessions@gmail.com', 
+// id: '733fbcc6-6e69-4eb4-94f3-b71283bbbe37'});
+// db.set('4b88c966-a392-410d-becb-8f34ca07ba6a',{firstName: 'Leah', lastName: 'Dvozak', emailAdd: 'leah.dvozak@gmail.com', 
+// id: '4b88c966-a392-410d-becb-8f34ca07ba6a'});
 
-const loadData = () => {};
-const saveData = () => {};
+const loadData = () => {
+    const jsonData = fs.readFileSync(path.join(__dirname, '../data/contacts.json'));
+    const contactsArray = JSON.parse(jsonData);
+    contactsArray.forEach(element => {
+        db.set(element[0], element[1], element[2], element[3]);
+    });
+};
+const saveData = () => {
+    const stringifyData = JSON.stringify(Array.from(db));
+    fs.writeFileSync(path.join(__dirname, '../data/contacts.json'), stringifyData);
+};
 
 
 const repo = {
@@ -23,9 +33,18 @@ const repo = {
 
          };
          db.set(newContact.id, newContact);
+         saveData();
     },
-    deleteById: (uuid) => db.delete(uuid),
-    update:(contact) => db.set(contact.id, contact),
+    deleteById: (uuid) => {
+        db.delete(uuid);
+        saveData();
+    },
+    update:(contact) => {
+        db.set(contact.id, contact);
+        saveData();
+    },
 };
+
+loadData();
 
 module.exports = repo;
